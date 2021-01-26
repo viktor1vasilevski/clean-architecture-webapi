0c972d87ceed97e3da396d735550990a0ab40532
viktor1vasilevski <vasilevskiviktor@yahoo.com>
2021-01-26 23:32:07 +0100

    adding comments for SaveChanges

--- CleanArchitectureWebAPI.Infrastructure.Data/Context/LibraryDbContext.cs
+++ CleanArchitectureWebAPI.Infrastructure.Data/Context/LibraryDbContext.cs
@@ -28,14 +28,19 @@ namespace CleanArchitectureWebAPI.Infrastructure.Data.Context
 
         public override int SaveChanges()
         {
+            // Get all the entities that inherit from AuditableEntity
+            // and have a state of Added or Modified
             var entries = ChangeTracker
                 .Entries()
                 .Where(e => e.Entity is AuditableBaseEntity && (
                         e.State == EntityState.Added
                         || e.State == EntityState.Modified));
 
+            // For each entity we will set the Audit properties
             foreach (var entityEntry in entries)
             {
+                // If the entity state is Added let's set
+                // the CreatedAt and CreatedBy properties
                 if (entityEntry.State == EntityState.Added)
                 {
                     ((AuditableBaseEntity)entityEntry.Entity).Created = DateTime.UtcNow;
@@ -43,13 +48,21 @@ namespace CleanArchitectureWebAPI.Infrastructure.Data.Context
                 }
                 else
                 {
+                    // If the state is Modified then we don't want
+                    // to modify the CreatedAt and CreatedBy properties
+                    // so we set their state as IsModified to false
                     Entry((AuditableBaseEntity)entityEntry.Entity).Property(p => p.Created).IsModified = false;
                     Entry((AuditableBaseEntity)entityEntry.Entity).Property(p => p.CreatedBy).IsModified = false;
                 }
 
+                // In any case we always want to set the properties
+                // ModifiedAt and ModifiedBy
                 ((AuditableBaseEntity)entityEntry.Entity).LastModified = DateTime.UtcNow;
                 ((AuditableBaseEntity)entityEntry.Entity).LastModifiedBy = this._httpContextAccessor?.HttpContext?.User?.Identity?.Name ?? "MyApp";
             }
+            // After we set all the needed properties
+            // we call the base implementation of SaveChanges
+            // to actually save our entities in the database
             return base.SaveChanges();
         }
     }
